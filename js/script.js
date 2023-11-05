70229a5550d0c0d0eb5495bdefcb6c70f2405788
jvbrian <brianjarabav@hotmail.com>
2023-11-04 22:10:51 -0500

    Se ha añadido el script

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Constantes
+const wrapper = document.querySelector(".wrapper"),
+  form = document.querySelector("form"),
+  fileInp = form.querySelector("input");
+
+// Función para mostrar la imágen
+form.addEventListener("click", () => {
+  alert("Tengo hambre");
+});
